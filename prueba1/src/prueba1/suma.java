27b474f002729bfcdd5ba78863616f40c612c6a6
Javierrope <javierrodriguez9698@gmail.com>
2022-04-27 09:38:59 +0200

    Commit de la clase suma.

---
+++ prueba1/src/prueba1/suma.java
@@ -0,0 +1,26 @@
+package prueba1;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class suma {
+
+	public static void main(String[] args) {
+		int num1, num2;
+		
+		// Hago el ejercicio con "sysos" por error en el Scanner, y en el
+		// JOptionPane, como te he comentado en el examen.
+		
+		System.out.println("Introduce el primer número");
+		num1 = 4;
+		
+		System.out.println("Introduce el segundo número");
+		
+		num2 = 6;
+		
+		System.out.println("La suma de los números introducidos es " + (num1+num2));
+		
+	}
+
+}
